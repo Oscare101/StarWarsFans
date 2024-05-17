ffba8da21d2ede722e3ff45704e8f4d8f4c1fec2
Oscare101 <kirillfedortsev2000@gmail.com>
2024-05-17 13:35:54 +0300

    like screen list characters

--- screens/LikesScreen.tsx
+++ screens/LikesScreen.tsx
@@ -1,18 +1,27 @@
 import {
+  FlatList,
   SafeAreaView,
   StyleSheet,
   Text,
   View,
   useColorScheme,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import Header from '../components/Header';
 import LikedPageSelectorBlock from '../components/LikedPageSelectorBlock';
 import colors from '../constants/colors';
 import {RootState} from '../redux';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import RenderCharacterItem from '../components/RenderCharacterItem';
+import {updateLikedCharacters} from '../redux/likedCharacters';
+import {
+  GetFilteredFemaleCharacters,
+  GetFilteredMaleCharacters,
+  GetFilteredOtherCharacters,
+  GetUpdatedLikedCharacters,
+} from '../functions/functions';
 
-export default function LikesScreen({route}: any) {
+export default function LikesScreen({navigation, route}: any) {
   const [page, setPage] = useState<string>(route.params.page);
 
   const systemTheme = useColorScheme();
@@ -21,6 +30,29 @@ export default function LikesScreen({route}: any) {
   const likedCharacters: any[] = useSelector(
     (state: RootState) => state.likedCharacters,
   );
+  const dispatch = useDispatch();
+
+  const LikeCharacter = useCallback(
+    (character: any) => {
+      dispatch(
+        updateLikedCharacters(
+          GetUpdatedLikedCharacters(likedCharacters, character),
+        ),
+      );
+    },
+    [likedCharacters],
+  );
+
+  function GetFilteredCharacters() {
+    switch (page) {
+      case 'male':
+        return GetFilteredMaleCharacters(likedCharacters);
+      case 'female':
+        return GetFilteredFemaleCharacters(likedCharacters);
+      case 'other':
+        return GetFilteredOtherCharacters(likedCharacters);
+    }
+  }
 
   return (
     <SafeAreaView
@@ -29,6 +61,25 @@ export default function LikesScreen({route}: any) {
       <LikedPageSelectorBlock
         page={page}
         onSelect={(value: string) => setPage(value)}
+      />
+
+      <FlatList
+        data={GetFilteredCharacters()}
+        renderItem={(item: any) => (
+          <RenderCharacterItem
+            item={item.item}
+            theme={themeColor}
+            onLikeCharacter={() => {
+              LikeCharacter(item.item);
+            }}
+            liked={likedCharacters.find((c: any) => c.name === item.item.name)}
+            onNavigation={() => {
+              navigation.navigate('CharacterInfoScreen', {
+                character: item.item,
+              });
+            }}
+          />
+        )}
       />
     </SafeAreaView>
   );
